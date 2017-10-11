a090ef8fc6f57ff6044d3beb6c51d0390e18e777
Aditya Dutta <aditya.dutta10@gmail.com>
2017-10-11 14:53:23 -0400

    Initial commit

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class BallScript : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed = 10f;
+
+    public GameObject plane;
+
+    public GameObject spawnPoint;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (transform.position.y < plane.transform.position.y - 10)
+        {
+
+            transform.position = spawnPoint.transform.position;
+
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float x = CrossPlatformInputManager.GetAxis("Horizontal");
+        float y = CrossPlatformInputManager.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(x, 0, y);
+        rb.velocity = move * speed;
+
+        if (x != 0 && y != 0)
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, Mathf.Atan2(x, y) * Mathf.Rad2Deg, transform.eulerAngles.z);
+        }
+    }
+}
